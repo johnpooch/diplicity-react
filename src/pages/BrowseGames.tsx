da134facd0f58ca77dd51520eb82df139ab22369
John McDowell <johnmcdowell@microsoft.com>
2024-12-12 11:54:32 +0000

    Add BrowseGames component and its Storybook stories

---
+++ src/pages/BrowseGames.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BrowseGames: React.FC = () => {
+  return (
+    <div>
+      <h1>Browse Games</h1>
+      <p>This is a placeholder for the BrowseGames component.</p>
+    </div>
+  );
+};
+
+export default BrowseGames;
