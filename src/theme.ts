b288f93a4325b49603399c2fe4378224d8ce3c33
John McDowell <johnmcdowell@microsoft.com>
2025-01-29 10:22:21 +0000

    Add Google Fonts and update theme typography to use 'Cabin' font

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
     typography: {
+        fontFamily: "'Cabin', sans-serif",
         h1: {
             fontSize: "18px",
             lineHeight: "22px",
