d80aa9631a638381e253f40b06924d83a0b90b69
Joren <j.bredman@gmail.com>
2025-02-07 20:04:09 +0100

    Quick basic login
    Made the login slightly better, added favicon and updated to the flat style Otto

--- src/screens/logged-out/login.tsx
+++ src/screens/logged-out/login.tsx
@@ -5,7 +5,7 @@ const getLoginUrl = (): string => {
   const redirectUrl = location.href;
   const tokenDuration = 60 * 60 * 24;
   return `https://diplicity-engine.appspot.com/Auth/Login?redirect-to=${encodeURI(
-    redirectUrl
+    redirectUrl,
   )}&token-duration=${tokenDuration}`;
 };
 
@@ -24,15 +24,33 @@ const Login: React.FC = () => {
       alignItems="center"
       height="100vh"
       bgcolor="#f5f5f5"
+      sx={{
+        backgroundImage: "url('/src/static/img/login_background.jpg')", // Change to your image path
+        backgroundSize: "cover",
+        backgroundPosition: "54%",
+        backgroundRepeat: "no-repeat",
+      }}
     >
-      <Stack sx={{ minWidth: 300, padding: 2 }} spacing={2} alignItems="center">
+      <Stack
+        sx={{
+          minWidth: 300,
+          padding: 2,
+          bgcolor: "#f5f5f5",
+          borderRadius: "4px",
+        }}
+        spacing={2}
+        alignItems="center"
+      >
         <img
           src="/otto.png"
           alt="Diplicity Logo"
           style={{ height: 48, width: 48 }}
         />
-        <Typography variant="h1" component="div" align="center">
+        <Typography variant="h1" component="div" align="left">
           Welcome to Diplicity!
+        </Typography>
+        <Typography variant="body1">
+          A digital adaptation of the game of Diplomacy.
         </Typography>
         <Box display="flex" justifyContent="center" mt={2}>
           <Button variant="contained" color="primary" onClick={onClickLogin}>
