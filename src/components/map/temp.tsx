b54d760112b74a66ef4fd2232775122f5bb1f452
John McDowell <johnmcdowell@microsoft.com>
2025-02-07 18:02:40 +0000

    s

---
+++ src/components/map/temp.tsx
@@ -0,0 +1,21 @@
+import { useMap } from "../../common";
+import { QueryContainer } from "../query-container";
+
+const Map: React.FC = () => {
+  const { query } = useMap();
+  return (
+    <QueryContainer query={query}>
+      {(data) => (
+        <div
+          dangerouslySetInnerHTML={{ __html: data }}
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+          }}
+        />
+      )}
+    </QueryContainer>
+  );
+};
+
+export { Map };
