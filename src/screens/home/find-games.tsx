aa3174343b26fa51d5e81634c3557d6f4f7b41c8
Joren <j.bredman@gmail.com>
2025-02-27 20:30:11 +0100

    Added an FAB for create game
    When the user uses mobile, the 'create game' option is removed from the side menu/bottom menu and added as a FAB to the game list.
    
    This saves main navigation space (and confusion) for an option that is not often used. We should actually try to NOT let people first make games, but join one first.
    
    On desktop we have more than enough real estate though, so can keep it in the side menu.

--- src/screens/home/find-games.tsx
+++ src/screens/home/find-games.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { List } from "@mui/material";
+import { List, Fab, useTheme, useMediaQuery } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
 import { QueryContainer } from "../../components";
 import { service } from "../../common";
 import { GameCard } from "../../components";
+import { useNavigate } from "react-router";
+
+const styles = {
+  fab: {
+    position: 'fixed',
+    bottom: 72,
+    right: 16,
+    zIndex: 1000,
+  },
+} as const;
 
 const options = { my: false, mastered: false };
 
@@ -17,13 +28,28 @@ const useFindGames = () => {
 
 const FindGames: React.FC = () => {
   const { query } = useFindGames();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const navigate = useNavigate();
 
   return (
-    <List sx={{ width: "100%" }} disablePadding>
-      <QueryContainer query={query}>
-        {(games) => games.map((game) => <GameCard key={game.ID} game={game} />)}
-      </QueryContainer>
-    </List>
+    <>
+      <List sx={{ width: "100%" }} disablePadding>
+        <QueryContainer query={query}>
+          {(games) => games.map((game) => <GameCard key={game.ID} game={game} />)}
+        </QueryContainer>
+      </List>
+      {isMobile && (
+        <Fab 
+          color="primary" 
+          sx={styles.fab}
+          onClick={() => navigate("/create-game")}
+          aria-label="create game"
+        >
+          <AddIcon />
+        </Fab>
+      )}
+    </>
   );
 };
 
