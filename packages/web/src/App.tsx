974cfd26c01df7780bf94bc820797f87b68136ff
John McDowell <johnmcdowell@microsoft.com>
2025-05-28 14:46:26 +0100

    Log environment variables

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -9,6 +9,12 @@ import { MessagingContextProvider } from "./context";
 import { store } from "./store";
 
 function App() {
+  // Log environment variables
+  console.log('Environment Variables:', {
+    VITE_GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    VITE_DIPLICITY_API_BASE_URL: import.meta.env.VITE_DIPLICITY_API_BASE_URL
+  });
+
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <Provider store={store}>
