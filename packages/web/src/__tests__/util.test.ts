08ce28bbd93ae3ab6d0bd84737db08e1d405f834
Johnpooch <johnmcdowell0801@gmail.com>
2025-08-28 15:55:03 +0100

    Remove files

--- packages/web/src/__tests__/util.test.ts
+++ packages/web/src/__tests__/util.test.ts
@@ -1,49 +1,8 @@
 import { describe, test } from "vitest";
-import { getOptions, transformResolution } from "../util";
+import { getOptions } from "../util";
 import { getOrderSummary } from "../util";
 import { getStepLabel } from "../util";
 import { Phase, Variant } from "../store";
-
-describe("transformResolution", () => {
-    test("OK", () => {
-        const resolution = "OK";
-        const result = transformResolution(resolution);
-        expect(result).toEqual({
-            outcome: "Succeeded",
-        });
-    })
-    test("ErrBounce:pie", () => {
-        const resolution = "ErrBounce:pie";
-        const result = transformResolution(resolution);
-        expect(result).toEqual({
-            outcome: "Bounced",
-            by: "pie",
-        });
-    })
-    test("ErrBounce:bul/ec", () => {
-        const resolution = "ErrBounce:bul/ec";
-        const result = transformResolution(resolution);
-        expect(result).toEqual({
-            outcome: "Bounced",
-            by: "bul/ec",
-        });
-    })
-    test("ErrSupportBroken:pie", () => {
-        const resolution = "ErrSupportBroken:pie";
-        const result = transformResolution(resolution);
-        expect(result).toEqual({
-            outcome: "Support broken",
-            by: "pie",
-        });
-    })
-    test("ErrInvalidSupporteeOrder", () => {
-        const resolution = "ErrInvalidSupporteeOrder";
-        const result = transformResolution(resolution);
-        expect(result).toEqual({
-            outcome: "Invalid order",
-        });
-    })
-});
 
 describe("getOrderSummary", () => {
     const mockVariant = {
