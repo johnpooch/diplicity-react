e0cddfb144919f0621c007ff204fea6d750b7b80
Johnpooch <johnmcdowell0801@gmail.com>
2025-09-28 15:34:13 +0100

    Fix admin for orders

--- service/order/admin.py
+++ service/order/admin.py
@@ -4,7 +4,25 @@ from .models import Order, OrderResolution
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["phase_state", "order_type", "source", "target", "aux"]
+    list_display = ["phase_state", "order_type", "source_name", "target_name", "aux_name"]
+
+    def source_name(self, obj):
+        return obj.source.name if obj.source else "-"
+
+    source_name.short_description = "Source"
+    source_name.admin_order_field = "source__name"
+
+    def target_name(self, obj):
+        return obj.target.name if obj.target else "-"
+
+    target_name.short_description = "Target"
+    target_name.admin_order_field = "target__name"
+
+    def aux_name(self, obj):
+        return obj.aux.name if obj.aux else "-"
+
+    aux_name.short_description = "Aux"
+    aux_name.admin_order_field = "aux__name"
 
 
 @admin.register(OrderResolution)
