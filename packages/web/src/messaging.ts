f32b10a4e1b3266de68c479c8e6663057a1e8a4f
Johnpooch <johnmcdowell0801@gmail.com>
2025-10-01 20:20:20 +0100

    Fix safari ios issue

--- packages/web/src/messaging.ts
+++ packages/web/src/messaging.ts
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { deleteToken, getMessaging, getToken, onMessage } from "firebase/messaging";
+import {
+  deleteToken,
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
 
 const VAPID_KEY =
   "BDzNEIDfAnaXQ3O6tZqAq2rtQw5lFDxhMHJYalOPHVpNXBLeWuuFCK42OdLHzVIBEIsjEEfxzGGQS2jcT3Wfa-8";
@@ -18,36 +24,69 @@ const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 const getFirebaseToken = async () => {
-  const registration = await navigator.serviceWorker.getRegistration();
-  if (!registration) {
-    throw new Error("Service worker registration not found");
-  }
-  const token = await getToken(messaging, { vapidKey: VAPID_KEY, serviceWorkerRegistration: registration });
-  return token;
-}
+  return isSupported()
+    .then(async supported => {
+      if (supported) {
+        const registration = await navigator.serviceWorker.getRegistration();
+        if (!registration) {
+          throw new Error("Service worker registration not found");
+        }
+        const token = await getToken(messaging, {
+          vapidKey: VAPID_KEY,
+          serviceWorkerRegistration: registration,
+        });
+        return token;
+      } else {
+        return null;
+      }
+    })
+    .catch(error => {
+      console.error("Failed to get Firebase token:", error);
+      return null;
+    });
+};
 
 const deleteFirebaseToken = async () => {
-  const registration = await navigator.serviceWorker.getRegistration();
-  if (!registration) {
-    throw new Error("Service worker registration not found");
-  }
-  await deleteToken(messaging);
-}
+  isSupported()
+    .then(async supported => {
+      if (supported) {
+        const registration = await navigator.serviceWorker.getRegistration();
+        if (!registration) {
+          throw new Error("Service worker registration not found");
+        }
+        await deleteToken(messaging);
+      }
+    })
+    .catch(error => {
+      console.error("Failed to delete Firebase token:", error);
+    });
+};
 
-navigator.serviceWorker
-  .register("/firebase-messaging-sw.js")
-  .then(() => {
-    console.log("Service worker registered successfully");
-  })
-  .catch((error) => {
-    console.error("Service worker registration failed:", error);
+const registerServiceWorker = () => {
+  return isSupported().then(async supported => {
+    if (supported) {
+      navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then(() => {
+          console.log("Service worker registered successfully");
+        })
+        .catch(error => {
+          console.error("Service worker registration failed:", error);
+        });
+    }
   });
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const onMessageReceived = (callback: (payload: any) => void) => {
-  onMessage(messaging, (payload) => {
+  onMessage(messaging, payload => {
     callback(payload);
   });
-}
+};
 
-export { getFirebaseToken as getToken, deleteFirebaseToken as deleteToken, onMessageReceived };
+export {
+  getFirebaseToken as getToken,
+  deleteFirebaseToken as deleteToken,
+  onMessageReceived,
+  registerServiceWorker,
+};
