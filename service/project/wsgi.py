3c27bbc094a911a3149aa2287350b26191a15cbc
John McDowell <john.mcdowell@intercom.io>
2025-10-20 16:05:23 +0100

    Add Honeycomb backend integration

--- service/project/wsgi.py
+++ service/project/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from project.otel_config import configure_opentelemetry
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+configure_opentelemetry()
 
 application = get_wsgi_application()
