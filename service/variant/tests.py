6fe1083b178718143ef35d9045ab9e569fd0d5b4
Johnpooch <johnmcdowell0801@gmail.com>
2025-10-25 15:36:01 +0100

    Add hundred variant

--- service/variant/tests.py
+++ service/variant/tests.py
@@ -16,7 +16,7 @@ def test_list_variants_success(authenticated_client, classical_variant):
     url = reverse(viewname)
     response = authenticated_client.get(url)
     assert response.status_code == status.HTTP_200_OK
-    assert len(response.data) == 2
+    assert len(response.data) == 3
     assert response.data[0]["id"] == classical_variant.id
     assert response.data[0]["name"] == classical_variant.name
     assert response.data[1]["id"] == "italy-vs-germany"
@@ -60,7 +60,7 @@ class TestVariantListViewQueryPerformance:
             response = authenticated_client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data) == 2
+        assert len(response.data) == 3
 
         # With optimized queries, we should have a constant number of queries
         # regardless of the number of variants due to prefetch_related
@@ -94,7 +94,7 @@ class TestVariantListViewQueryPerformance:
 
         assert response.status_code == status.HTTP_200_OK
         # Should have at least 3 variants now (classical, italy-vs-germany, test-single-variant)
-        assert len(response.data) >= 3
+        assert len(response.data) >= 4
 
         # Should have the same number of queries regardless of variant count
         # due to prefetch_related optimization
