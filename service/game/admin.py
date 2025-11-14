06dd994540a8668dceb65f4b81b2f2f2ac976a26
John McDowell <john.mcdowell@intercom.io>
2025-11-14 11:29:51 +0000

    Enhance GameAdmin to include a link for viewing associated phases

--- service/game/admin.py
+++ service/game/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.contrib import messages
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Game
 from common.constants import PhaseStatus
 
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "variant", "status", "current_phase", "private", "movement_phase_duration")
+    list_display = ("id", "name", "variant", "status", "current_phase", "view_phases", "private", "movement_phase_duration")
     list_filter = ("status", "private", "variant")
     search_fields = ("name", "id")
     actions = ["start_game", "resolve_game"]
@@ -23,3 +25,14 @@ class GameAdmin(admin.ModelAdmin):
             return f"{phase.name} (#{phase.ordinal})"
         return "-"
     current_phase.short_description = "Current Phase"
+
+    def view_phases(self, obj):
+        """Display a link to view all phases for this game."""
+        phases = obj.phases.all()
+        count = len(phases)
+        if count == 0:
+            return "-"
+        
+        url = reverse("admin:phase_phase_changelist") + f"?game__id__exact={obj.id}"
+        return format_html('<a href="{}">{} phases</a>', url, count)
+    view_phases.short_description = "Phases"
